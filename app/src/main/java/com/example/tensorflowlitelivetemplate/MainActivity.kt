4976b676c558774f7dbc2e4cfeb7a6ccf6e5aaf8
Andreas Urlberger <A.Urlberger@outlook.com>
2022-05-09 19:31:40 +0200

    Setup camera preview and converts analyzer image from yuv to argb

---
+++ app/src/main/java/com/example/tensorflowlitelivetemplate/MainActivity.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.tensorflowlitelivetemplate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.example.tensorflowlitelivetemplate.databinding.ActivityMainBinding
+
+private const val IMMERSIVE_FLAG_TIMEOUT = 500L
+
+/**
+ * Main entry point into our app. This app follows the single-activity pattern, and all
+ * functionality is implemented in the form of fragments.
+ */
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+    }
+
+}
