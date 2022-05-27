695c4756d6d6ec26e4e18b2b55cd4d330e54fdff
Andreas Urlberger <A.Urlberger@outlook.com>
2022-05-27 18:54:04 +0200

    End of presentation result

--- app/src/main/java/com/example/tensorflowlitelivecoding/Analyzer.kt
+++ app/src/main/java/com/example/tensorflowlitelivecoding/Analyzer.kt
@@ -2,10 +2,33 @@ package com.example.tensorflowlitelivecoding
 
 import android.content.Context
 import android.graphics.Bitmap
+import androidx.core.graphics.scale
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.vision.detector.Detection
+import org.tensorflow.lite.task.vision.detector.ObjectDetector
 
 class Analyzer(context: Context, private val drawer: BoundingBoxDrawer) {
 
-    fun analyze(bitmap: Bitmap) {
+    private val cropWidth = 300
+    private val cropHeight = 300
 
+    private val detector: ObjectDetector
+
+    init {
+        val options =
+            ObjectDetector.ObjectDetectorOptions.builder().setMaxResults(5).setScoreThreshold(0.5f)
+                .build()
+        detector = ObjectDetector.createFromFileAndOptions(context, "detect.tflite", options)
+    }
+
+    fun analyze(bitmap: Bitmap) {
+        val cropped = bitmap.scale(cropWidth, cropHeight)
+        val tensorImage = TensorImage.fromBitmap(cropped)
+        val results = detector.detect(tensorImage)
+        val boxes = results.map(Detection::getBoundingBox)
+        val labels = results.map { result ->
+            "${result.categories.first().label}(%.2f)".format(result.categories.first().score)
+        }
+        drawer.drawBoundingBoxes(boxes, labels)
     }
 }
